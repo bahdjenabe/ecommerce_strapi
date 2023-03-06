3a468e623b8165e5a116ad7762779fabcd5a28a8
Bah Djenabou <djenaboulim@gmail.com>
2023-03-06 11:29:53 +0100

    requête paiement

--- client/src/components/Cart/Cart.js
+++ client/src/components/Cart/Cart.js
@@ -3,9 +3,27 @@ import { MdClose } from "react-icons/md";
 import { BsCartX } from "react-icons/bs";
 import CartItem from "./CartItem/CartItem";
 import { Context } from "../../utils/context";
+import { loadStripe } from "@stripe/stripe-js";
+import { makePayementRequest } from "../../utils/api";
 
 const Cart = ({ setOpenCart }) => {
   const { cartItems, cartSubTotal } = useContext(Context);
+
+  const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PUBLISHABLE);
+  const handlePayment = async () => {
+    try {
+      const stripe = await stripePromise;
+      const resp = await makePayementRequest.post("/api/orders", {
+        products: cartItems,
+      });
+
+      await stripe.redirectToCheckout({
+        sessionId: resp.data.stripeSession.id,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <div className="cart">
       <div className="cart-opacity"></div>
@@ -37,7 +55,10 @@ const Cart = ({ setOpenCart }) => {
               </div>
 
               <div className="button">
-                <button className="verifier"> Vérifier</button>
+                <button className="verifier" onClick={handlePayment}>
+                  {" "}
+                  Vérifier
+                </button>
               </div>
             </div>
           </>
